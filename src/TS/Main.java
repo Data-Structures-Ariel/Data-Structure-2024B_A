0ae68bc647a3b446afdff85b4272718caacf0619
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2024-09-05 14:46:05 +0300

    up

--- src/TS/Main.java
+++ src/TS/Main.java
@@ -2,6 +2,8 @@ package TS;
 
 import Nodes.Node;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -9,12 +11,18 @@ public class Main {
         head.setNext(new Node(21));
         head.getNext().setNext(new Node(12));
         head.getNext().getNext().setNext(new Node(4));
-        head.getNext().getNext().getNext().setNext(new Node(3));
+        head.getNext().getNext().getNext().setNext(new Node(1));
         head.getNext().getNext().getNext().getNext().setNext(new Node(2));
         head.getNext().getNext().getNext().getNext().getNext().setNext(new Node(8));
         head.getNext().getNext().getNext().getNext().getNext().getNext().setNext(new Node(6));
-        head.getNext().getNext().getNext().getNext().getNext().getNext().getNext().setNext(new Node(22));
-        head.getNext().getNext().getNext().getNext().getNext().getNext().getNext().getNext().setNext(new Node(2));
+        head.getNext().getNext().getNext().getNext().getNext().getNext().getNext().setNext(new Node(2));
+        head.getNext().getNext().getNext().getNext().getNext().getNext().getNext().getNext().setNext(new Node(21));
+        head.getNext().getNext().getNext().getNext().getNext().getNext().getNext().getNext().getNext().setNext(new Node(28));
+        head.getNext().getNext().getNext().getNext().getNext().getNext().getNext().getNext().getNext().getNext().setNext(new Node(20));
+
+
+        boolean bool = chainK(head, 2);
+        System.out.println(bool);
 
         if (head == null)
             System.out.println("return / break ");
@@ -35,15 +43,117 @@ public class Main {
                         new Node<>(543)));
         print(ch);
 
-
         print(chain);
 
         Node<Integer> digits = chainDigit(chain);
         print(digits);
 
 
-        boolean bool = chainK(chain,2);
+        IA[] allA = {new Boo(), new Foo(3), new Goo(3)};
+        IB b = new Goo(3);
+        b.B();
+        IA f = (Foo) allA[1];
+        f = (Boo) allA[0];
+        ((Boo) f).Go();
 
+
+        IC c = new Goo(4);
+
+
+    }
+
+    static class PriorityQueue {
+        private Queue<Student> students;
+        private int size;
+
+        public PriorityQueue() {
+            students = new LinkedList<>();
+            size = 0;
+        }
+
+        public boolean add(Student s) {
+            boolean bool = true;
+            if (students.isEmpty())
+                students.add(s);
+
+            else
+                for (int i = 0; i < size; i++) {
+                    if (students.peek().getAverage() < s.getAverage())
+                        students.add(students.poll());
+                    else if (bool) {
+                        students.add(s);
+                        students.add(students.poll());
+                        size++;
+                        bool = !bool;
+                    }
+
+
+                }
+
+            return true;
+        }
+
+        public double remove() {
+            if (students.isEmpty())
+                return -1;
+            size--;
+            for (int i = 0; i < size; i++) {
+                students.add(students.poll());
+            }
+            Student s = students.remove();
+            return s.getAverage();
+        }
+    }
+
+    static class Student {
+        private String name;
+        private Node<Integer> grades;
+
+        public double addGrade(int grade) {
+
+            if (grades == null)
+                grades = new Node<>(grade);
+            else {
+                Node<Integer> newGrade = new Node<>(grade);
+                newGrade.setNext(grades);
+                grades = newGrade;
+            }
+            return grade;
+
+        }
+
+        public double getAverage() {
+            if (grades == null)
+                return 0;
+            Node<Integer> temp = grades;
+            int sum = 0;
+            int count = 0;
+
+            while (temp != null) {
+                count++;
+                sum += temp.getValue();
+                temp = temp.getNext();
+
+            }
+            return (double) sum / count;
+
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Node<Integer> getGrades() {
+            return grades;
+        }
+
+        public void setGrades(Node<Integer> grades) {
+            this.grades = grades;
+        }
     }
 
     private static boolean chainK(Node<Integer> chain, int k) {
@@ -74,7 +184,7 @@ public class Main {
                         return false;
                     chain = chain.getNext();
                 }
-            flag=!flag;
+            flag = !flag;
             chain = chain.getNext();
         }
 
@@ -151,5 +261,70 @@ public class Main {
             return next != null;
         }
     }
+
+
+    interface IA {
+        void A();
+    }
+
+    interface IB {
+        void B();
+    }
+
+    interface IC extends IA {
+        void C();
+    }
+
+    static class Boo extends Foo implements IB {
+        public Boo() {
+            super(2);
+        }
+
+        public void A() {
+        }
+
+        public void B() {
+        }
+
+        public void Go() {
+        }
+    }
+
+    static class Foo implements IA {
+        int num;
+
+        public Foo(int num) {
+            this.num = num;
+        }
+
+        @Override
+        public void A() {
+
+        }
+    }
+
+    static class Goo implements IC, IB {
+        int num;
+
+        public Goo(int num) {
+            this.num = num;
+        }
+
+        @Override
+        public void A() {
+
+        }
+
+        @Override
+        public void B() {
+
+        }
+
+        @Override
+        public void C() {
+
+        }
+    }
+
 
 }
